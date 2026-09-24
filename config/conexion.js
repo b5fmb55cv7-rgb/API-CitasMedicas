c3642d3a3221b2d4185138f3e46adbfd62e1a8b0
Juanes <b5fm55vcv7@privaterelay.appleid.com>
2026-09-23 19:21:55 -0500

    Usar pool de conexiones MySQL

--- config/conexion.js
+++ config/conexion.js
@@ -2,19 +2,24 @@ const mysql = require("mysql2");
 
 require("dotenv").config();
 
-const conexion = mysql.createConnection({
+const conexion = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    port: Number(process.env.DB_PORT),
+    waitForConnections: true,
+    connectionLimit: 5,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 10000
 });
 
-conexion.connect((error) => {
+conexion.getConnection((error, conn) => {
     if (error) {
-        console.error("❌ Error de conexión:", error);
+        console.error("❌ Error de conexión:", error.message);
     } else {
         console.log("✅ Base de datos conectada correctamente.");
+        conn.release();
     }
 });
 
